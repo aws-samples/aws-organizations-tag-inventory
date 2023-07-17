27f4d20d0e7e7281015ddf599f19b8ee1c4b4892
Emma Kunzova <ekunzova@amazon.com>
2023-07-17 14:02:35 -0400

    Update with new permissions

--- src/constructs/StateMachineFromFile.ts
+++ src/constructs/StateMachineFromFile.ts
@@ -1,29 +1,29 @@
 import * as fs from 'fs';
-import {IFunction} from 'aws-cdk-lib/aws-lambda';
-import {CfnStateMachine, Pass, StateMachine} from 'aws-cdk-lib/aws-stepfunctions';
-import {Construct} from 'constructs';
+import { IFunction } from 'aws-cdk-lib/aws-lambda';
+import { CfnStateMachine, Pass, StateMachine } from 'aws-cdk-lib/aws-stepfunctions';
+import { Construct } from 'constructs';
 
 export interface StateMachineFromFileConfig {
-	name: string;
-	file: string;
-	searchFunction: IFunction;
-	mergeFunction: IFunction
+  name: string;
+  file: string;
+  searchFunction: IFunction;
+  mergeFunction: IFunction;
 }
 
 export class StateMachineFromFile extends Construct {
 
-	readonly stateMachine: StateMachine;
+  readonly stateMachine: StateMachine;
 
-	constructor(scope: Construct, id: string, config: StateMachineFromFileConfig) {
-		super(scope, id);
-		this.stateMachine = new StateMachine(this, config.name, {
-			definition: new Pass(this, 'StartState'),
-		});
+  constructor(scope: Construct, id: string, config: StateMachineFromFileConfig) {
+    super(scope, id);
+    this.stateMachine = new StateMachine(this, config.name, {
+      definition: new Pass(this, 'StartState'),
+    });
 
-		const cfnStatemachine = this.stateMachine.node.defaultChild as CfnStateMachine;
-		const buffer = fs.readFileSync(config.file.toString());
-		cfnStatemachine.definitionString = buffer.toString();
-		cfnStatemachine.definitionSubstitutions = {SEARCH_FUNCTION: config.searchFunction.functionArn, MERGE_FUNCTION: config.mergeFunction.functionArn};
-	}
+    const cfnStatemachine = this.stateMachine.node.defaultChild as CfnStateMachine;
+    const buffer = fs.readFileSync(config.file.toString());
+    cfnStatemachine.definitionString = buffer.toString();
+    cfnStatemachine.definitionSubstitutions = { SEARCH_FUNCTION: config.searchFunction.functionArn, MERGE_FUNCTION: config.mergeFunction.functionArn };
+  }
 
 }
--- src/functions/MergeResults.ts
+++ src/functions/MergeResults.ts
@@ -1,41 +1,48 @@
 export const onEvent = async (
-	event: any,
-	//@ts-ignore
-	context: Context,
-	//@ts-ignore
-	callback: Callback,
+  event?: any,
+  //@ts-ignore
+  context: Context,
+  //@ts-ignore
+  callback: Callback,
 ): Promise<string> => {
-	console.log(`Event: ${JSON.stringify(event)}`);
-	const flatten: Object[] = event.Results.flatten
-	let result: { [key: string]: any } = event!=undefined ? event.PreviousResults : {}
-	flatten.forEach((value: { [key: string]: any }) => {
-		const tagName = Object.keys(value)[0]
-		const tagValue = Object.keys(value[tagName] as {})[0]
-		const resources = value[tagName][tagValue]
-		if (tagName in result) {
-			const existingValue = result[tagName]
-			if (tagValue in existingValue) {
-				const valueArray = existingValue[tagValue]
-				if (valueArray == undefined) {
-					existingValue[tagValue] = []
-				}
-				existingValue[tagValue] = valueArray.concat(resources)
-			} else {
-				//@ts-ignore
-				value[tagName][tagValue] = resources
-			}
-		} else {
-			result = {
-				...result,
-				...value
-			}
-		}
-	})
+  console.log(`Event: ${JSON.stringify(event)}`);
+  const flatten: Object[] = event.Results.flatten;
+  let result: { [key: string]: any } = event.PreviousResults!=undefined ? event.PreviousResults : {};
+  flatten.forEach((value: { [key: string]: any }) => {
+    const tagName = Object.keys(value)[0];
+    const tagValue = Object.keys(value[tagName] as {})[0];
+    const resources = value[tagName][tagValue];
+    if (tagName in result) {
+      const existingValue = result[tagName];
+      if (tagValue in existingValue) {
+        const valueArray = existingValue[tagValue];
+        if (valueArray == undefined) {
+          existingValue[tagValue] = [];
+        }
+        existingValue[tagValue] = valueArray.concat(resources);
+      } else {
+        //@ts-ignore
+        value[tagName][tagValue] = resources;
+      }
+    } else {
+      result = {
+        ...result,
+        ...value,
+      };
+    }
+  });
+  // @ts-ignore
+  const resultArray: [{ [key: string]: any }] = [];
+  for (const k of Object.keys(result)) {
+    const newResult: { [key: string]: any } = {};
+    newResult[k]=result[k];
+    resultArray.push(newResult);
 
-	return JSON.stringify({
-		"NextToken": event?.Payload?.Result?.NextToken,
-		"Results": result
-	});
+  }
+  return JSON.stringify({
+    NextToken: event?.Payload?.Result?.NextToken,
+    Results: resultArray,
+  });
 
 
 };
