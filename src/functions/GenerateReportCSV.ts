7990e21aa0881211ac9e276ba5e77279b2a1ba15
Galen Dunkleberger <awsgalen@amazon.com>
2023-08-03 10:53:01 -0400

    finally got permission scoped down

--- src/functions/GenerateReportCSV.ts
+++ src/functions/GenerateReportCSV.ts
@@ -36,64 +36,55 @@ export const onEvent = async (
 	const yesterday = new Date(today.setDate(today.getDate() - 1));
 	const yesterdayString = yesterday.toISOString().substring(0, 10);
 	const client = new AthenaClient({});
-	const loadParitionsResponse=await loadPartitions(client);
-	if(loadParitionsResponse!=undefined) {
+	const loadPartitionsResponse = await loadPartitions(client);
+	if (loadPartitionsResponse != undefined && loadPartitionsResponse.QueryExecution?.Status?.State == QueryExecutionState.SUCCEEDED) {
 		const createTableResponse = await createTable(client, yesterdayString)
-		if (createTableResponse != undefined) {
-			try {
-
-				if (QueryExecutionState.SUCCEEDED == createTableResponse.QueryExecution?.Status?.State) {
-					logger.info(`Create table succeeded: ${JSON.stringify(createTableResponse)}`);
-					const manifestUri = new URL(createTableResponse.QueryExecution.Statistics?.DataManifestLocation!);
-					const manifestBucket = manifestUri.hostname;
-					const manifestKey = manifestUri.pathname.substring(1);
-					const s3Client = new S3Client({});
-					logger.info(`Retrieving manifest from : ${manifestBucket}/${manifestKey}`);
-					const getObjectResponse = await s3Client.send(new GetObjectCommand({
-						Bucket: manifestBucket,
-						Key: manifestKey,
-					}));
-					const dataFileLocation = await getObjectResponse.Body?.transformToString();
-					if (dataFileLocation != undefined) {
-						logger.info(`Data file located at: '${dataFileLocation}'`);
-						const dataFileUri = new URL(dataFileLocation);
-						const dataFileBucket = dataFileUri.hostname;
-						const dataFileKey = dataFileUri.pathname.substring(1);
-						await s3Client.send(new CopyObjectCommand({
-							CopySource: `${dataFileBucket}/${dataFileKey}`,
-							Key: `tag-inventory-${yesterdayString}.csv.gz`,
-							Bucket: process.env.REPORT_BUCKET,
-						}));
-						await s3Client.send(new DeleteObjectCommand({
-							Bucket: dataFileBucket,
-							Key: dataFileKey,
-						}));
-					} else {
-						throw new Error("Could not determine data file location: '${dataFileLocation}'");
-					}
-					logger.info('Dropping table');
-					await client.send(new StartQueryExecutionCommand({
-						QueryString: `drop table tag_inventory_csv`,
-						WorkGroup: process.env.WORKGROUP,
-					}));
-					await s3Client.send(new DeleteObjectCommand({
-						Bucket: manifestBucket,
-						Key: 'tables',
-					}));
-				} else {
-					throw new Error(`Create table failed: ${JSON.stringify(createTableResponse.QueryExecution?.Status?.AthenaError)}`);
-
-				}
-			} catch (e) {
-				const error = e as Error;
-				throw new Error(`Error: ${JSON.stringify(error)}`);
-
+		if (createTableResponse != undefined && QueryExecutionState.SUCCEEDED == createTableResponse.QueryExecution?.Status?.State) {
+			logger.info(`Create table succeeded: ${JSON.stringify(createTableResponse)}`);
+			const manifestUri = new URL(createTableResponse.QueryExecution.Statistics?.DataManifestLocation!);
+			const manifestBucket = manifestUri.hostname;
+			const manifestKey = manifestUri.pathname.substring(1);
+			const s3Client = new S3Client({});
+			logger.info(`Retrieving manifest from : ${manifestBucket}/${manifestKey}`);
+			const getObjectResponse = await s3Client.send(new GetObjectCommand({
+				Bucket: manifestBucket,
+				Key: manifestKey,
+			}));
+			const dataFileLocation = await getObjectResponse.Body?.transformToString();
+			if (dataFileLocation != undefined) {
+				logger.info(`Data file located at: '${dataFileLocation}'`);
+				const dataFileUri = new URL(dataFileLocation);
+				const dataFileBucket = dataFileUri.hostname;
+				const dataFileKey = dataFileUri.pathname.substring(1);
+				await s3Client.send(new CopyObjectCommand({
+					CopySource: `${dataFileBucket}/${dataFileKey}`,
+					Key: `tag-inventory-${yesterdayString}.csv.gz`,
+					Bucket: process.env.REPORT_BUCKET,
+				}));
+				await s3Client.send(new DeleteObjectCommand({
+					Bucket: dataFileBucket,
+					Key: dataFileKey,
+				}));
+			} else {
+				throw new Error("Could not determine data file location: '${dataFileLocation}'");
 			}
-		}else{
-			throw new Error("No response from create table command")
+			logger.info('Dropping table');
+			const dropTableResponse = await executeCommand(client, `DROP TABLE \`${process.env.DATABASE}.tag_inventory_csv\`;`)
+			if (dropTableResponse != undefined && QueryExecutionState.SUCCEEDED == dropTableResponse.QueryExecution?.Status?.State) {
+				logger.info(`Deleting tables from ${manifestBucket}`);
+				await s3Client.send(new DeleteObjectCommand({
+					Bucket: manifestBucket,
+					Key: 'tables',
+				}));
+			} else {
+				throw new Error("Could not drop table")
+			}
+
+		} else {
+			throw new Error("Could not create table")
 		}
-	}else {
-		throw new Error("No response from load partitions command")
+	} else {
+		throw new Error(`Could not load partitions`)
 
 	}
 
@@ -103,11 +94,11 @@ export const onEvent = async (
 };
 
 async function loadPartitions(client: AthenaClient): Promise<GetQueryExecutionCommandOutput | undefined> {
-	logger.info('Creating table');
+	logger.info('Loading partitions');
 	const loadPartitions = `MSCK REPAIR TABLE \`${process.env.TAG_INVENTORY_TABLE}\`;`;
-
-	return await executeCommand(client,loadPartitions)
+	return await executeCommand(client, loadPartitions)
 }
+
 async function createTable(client: AthenaClient, yesterdayString: string): Promise<GetQueryExecutionCommandOutput | undefined> {
 	logger.info('Creating table');
 	const createTable = `CREATE TABLE \"${process.env.DATABASE}\".\"tag_inventory_csv\"\n` +
@@ -119,34 +110,31 @@ async function createTable(client: AthenaClient, yesterdayString: string): Promi
 		'      bucket_count = 1)\n' +
 		`AS (\n SELECT d,tagname,tagvalue,r.owningAccountId,r.region,r.service,r.resourceType,r.arn FROM \"${process.env.DATABASE}\".\"${process.env.TAG_INVENTORY_TABLE}\", unnest(\"resources\") as t (\"r\") where d='${yesterdayString}'\n);`;
 
-	return await executeCommand(client,createTable)
+	return await executeCommand(client, createTable)
 }
 
-async function executeCommand(client:AthenaClient,command:string):Promise<GetQueryExecutionCommandOutput | undefined>{
+async function executeCommand(client: AthenaClient, command: string): Promise<GetQueryExecutionCommandOutput | undefined> {
 	const input: StartQueryExecutionCommandInput = { // StartQueryExecutionInput
 		QueryString: command, // required
 		WorkGroup: process.env.WORKGROUP,
 	};
 	let response: GetQueryExecutionCommandOutput | undefined = undefined;
-	try {
-		logger.info('Creating table');
-		const startQueryExecutionResponse = await client.send(new StartQueryExecutionCommand(input));
-		let sleepLoopCount = 0
-		while (response == undefined || ['RUNNING', 'QUEUED'].includes(response.QueryExecution?.Status?.State!)) {
-			// @ts-ignore
-			response = await client.send(new GetQueryExecutionCommand({
-				QueryExecutionId: startQueryExecutionResponse?.QueryExecutionId,
-			}));
-			logger.info(`Status: ${response.QueryExecution?.Status?.State}`);
-			await sleep(3000);
-			if (sleepLoopCount++ > 5) {
-				throw new Error("Create table query took too long")
-			}
+	const startQueryExecutionResponse = await client.send(new StartQueryExecutionCommand(input));
+	let sleepLoopCount = 0
+	while (response == undefined || ['RUNNING', 'QUEUED'].includes(response.QueryExecution?.Status?.State!)) {
+		// @ts-ignore
+		response = await client.send(new GetQueryExecutionCommand({
+			QueryExecutionId: startQueryExecutionResponse?.QueryExecutionId,
+		}));
+		if (QueryExecutionState.FAILED == response.QueryExecution?.Status?.State) {
+			logger.error(`${JSON.stringify(response.QueryExecution?.Status?.AthenaError)}`)
+		} else {
+			logger.info(`Response: ${JSON.stringify(response)}`);
 		}
-
-	} catch (e) {
-		const error = e as Error;
-		logger.error(`Error: ${JSON.stringify(error)}`);
+		await sleep(3000);
+		if (sleepLoopCount++ > 5) {
+			throw new Error("Create table query took too long")
+		}
 	}
 	return response
 }
